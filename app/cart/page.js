56c1c3cccad406ae9cf1df88ae1aa183fb6dc6e5
nijanth-ts <nijanth.ts@gmail.com>
2025-11-19 11:26:31 +0530

    navigation added from cart to pdp page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useCart } from "../../context/CartContext";
 import QtyButton from "./QtyButtton";
 
@@ -32,13 +33,17 @@ export default function CartPage() {
               key={item.id}
               className="flex items-center gap-5 border p-4 rounded-lg shadow-sm"
             >
-              <img
-                src={item.thumbnail}
-                alt={item.title}
-                className="h-20 w-20 object-cover rounded-md"
-              />
+              <Link href={`/products/${item.id}`}>
+                <img
+                  src={item.thumbnail}
+                  alt={item.title}
+                  className="h-20 w-20 object-cover rounded-md"
+                />
+              </Link>
               <div className="flex-1">
-                <h2 className="font-semibold text-lg">{item.title}</h2>
+                <Link href={`/products/${item.id}`}>
+                  <h2 className="font-semibold text-lg">{item.title}</h2>
+                </Link>
                 <p className="text-gray-600">${item.price}</p>
 
                 <div className="flex items-center gap-3 mt-2">
